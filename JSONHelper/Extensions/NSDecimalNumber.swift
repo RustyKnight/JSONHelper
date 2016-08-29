3b7cdf4ebc751924348d09597d98da96408cb36b
Shane Whitehead <shane.whitehead@beamcommunications.com>
2016-08-29 17:13:47 +1000

    Xcode8b6

--- JSONHelper/Extensions/NSDecimalNumber.swift
+++ JSONHelper/Extensions/NSDecimalNumber.swift
@@ -5,20 +5,20 @@
 import Foundation
 
 extension NSDecimalNumber: Convertible {
-
-  public static func convertFromValue<T>(value: T?) throws -> Self? {
-    guard let value = value else { return nil }
-
-    if let doubleValue = value as? Double {
-      return self.init(double: doubleValue)
-    } else if let stringValue = value as? String {
-      return self.init(string: stringValue)
-    } else if let floatValue = value as? Float {
-      return self.init(float: floatValue)
-    } else if let intValue = value as? Int {
-      return self.init(integer: intValue)
+    
+    public static func convertFromValue<T>(_ value: T?) throws -> Self? {
+        guard let value = value else { return nil }
+        
+        if let doubleValue = value as? Double {
+            return self.init(decimal: Decimal(doubleValue))
+        } else if let stringValue = value as? String {
+            return self.init(string: stringValue)
+        } else if let floatValue = value as? Float {
+            return self.init(decimal: Decimal(Double(floatValue)))
+        } else if let intValue = value as? Int {
+            return self.init(decimal: Decimal(intValue))
+        }
+        
+        throw ConversionError.unsupportedType
     }
-
-    throw ConversionError.UnsupportedType
-  }
 }
